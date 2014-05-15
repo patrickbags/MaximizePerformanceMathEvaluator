7e9837ba30c053a2a4ac0b6a933bc17025b625cf
shwetaskatdare <shwetaskatdare@gmail.com>
2014-05-15 16:26:33 -0400

    Update Binary_Expr_Node.h

--- Binary_Expr_Node.h
+++ Binary_Expr_Node.h
@@ -1,11 +1,3 @@
-//==============================================================================
-/** 
-* Honor Pledge:
- *
- * I pledge that I have neither given nor received any help
- * on this assignment.
- */
-//==============================================================================
 
 /**
  * @class Binary_Expr_Node
